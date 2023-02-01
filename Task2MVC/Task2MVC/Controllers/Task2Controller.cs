c453f7268124a2e3ae627537ccefe29481881157
Mo'ath Bdour <moathbdour@users.noreply.github.com>
2023-02-01 15:53:39 +0300

    task2

---
+++ Task2MVC/Task2MVC/Controllers/Task2Controller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task2MVC.Models;
+
+namespace Task2MVC.Controllers
+{
+    public class Task2Controller : Controller
+    {
+        // GET: Task2
+        public ActionResult Index()
+        {
+            List<Models.Student> moath = new List<Models.Student>();
+            moath.Add(new Models.Student
+            {
+                ID= 1,
+                Name="moath",
+                Facility="Engineer",
+                Major="Civil Engineering"
+            });
+            moath.Add(new Models.Student
+            {
+                ID = 2,
+                Name = "Ahmad",
+                Facility = "Engineer",
+                Major = "Not Engineering"
+            });
+            return View(moath);
+        }
+       
+    }
+}
